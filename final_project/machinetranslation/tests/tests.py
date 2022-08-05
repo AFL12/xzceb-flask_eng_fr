c082415e969d3e0fb745fdb635838dca3754341d
AFL12 <alberto.lelli@hotmail.it>
2022-08-05 00:50:08 +0000

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,8 @@
+import translator 
+
+
+print(translator.french_to_english('Bonjour'))
+print(translator.english_to_french('Hello'))
+print(translator.french_to_english())
+print(translator.english_to_french())
+
